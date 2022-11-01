a9807d5cf4b0d6781312e354c37eb7e64abd0f80
Misha <70710273+MbNextT1me@users.noreply.github.com>
2022-11-01 09:42:52 +0800

    Add files via upload

---
+++ ilib/ilib/tokens.py
@@ -0,0 +1,20 @@
+# Tokens.py
+from enum import Enum, auto
+
+class TokenType(Enum):
+    NUMBER = auto()
+    PLUS = auto()
+    MINUS = auto()
+    DIV = auto()
+    MUL = auto()
+    EOL = auto()
+
+
+class Token:
+
+    def __init__(self, type_:TokenType, value: str):
+        self.type = type_
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Token ({self.type}, {self.value})"
